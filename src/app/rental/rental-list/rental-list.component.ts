cf91c73d2b6e267dcff1d433d5e495a73a5e6ca8
Rushabh Shah <shahrushabh87@gmail.com>
2020-05-09 21:46:37 +0800

    ngFor iteratiion, passing data from parent to child page

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -7,6 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentalListComponent implements OnInit {
 
+  rentals: any[] = [{
+  	id: 1,
+  	title: "Central Apartment 1",
+  	city: "New York",
+  	street : "Times Square",
+  	category: "Apartment",
+  	image: "http://via.placeholder.com/350x250",
+  	bedrooms: 3,
+  	description: "Very Nice Aparment",
+  	dailyRate: 24,
+  	shared: false,
+  	createdAt: '24/12/2017'
+
+  },
+  {
+  	id: 2,
+  	title: "Central Apartment 2",
+  	city: "San fransico",
+  	street : "Mains street Square",
+  	category: "Condo",
+  	image: "http://via.placeholder.com/350x250",
+  	bedrooms: 2,
+  	description: "Very Nice Aparment",
+  	dailyRate: 34,
+  	shared: true,
+  	createdAt: '24/12/2017'
+
+  },
+  {
+  	id: 3,
+  	title: "Central Apartment 3",
+  	city: "Berlin",
+  	street : "Residal trait",
+  	category: "Apartment",
+  	image: "http://via.placeholder.com/350x250",
+  	bedrooms: 5,
+  	description: "Very Nice Aparment",
+  	dailyRate: 12,
+  	shared: true,
+  	createdAt: '24/12/2017'
+
+  },
+  {
+  	id: 4,
+  	title: "Central Apartment 4",
+  	city: "Bratislva",
+  	street : "Wakeruni Brait",
+  	category: "Apartment",
+  	image: "http://via.placeholder.com/350x250",
+  	bedrooms: 4,
+  	description: "Very Nice Aparment",
+  	dailyRate: 43,
+  	shared: false,
+  	createdAt: '24/12/2017'
+
+  }];
+
   constructor() { }
 
   ngOnInit() {
