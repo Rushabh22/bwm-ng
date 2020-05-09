1562d32592ee63ede98f9f77b326ff9a0f71f312
Rushabh Shah <shahrushabh87@gmail.com>
2020-05-09 19:58:28 +0800

    Fix the component file

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'bwm-header',
-	templateUrl:'./header.Component.html'
+	templateUrl:'./header.Component.html',
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
