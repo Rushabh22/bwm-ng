44ddb9a0b2ae0ce2d0cdffdb20e0f249dd254f31
Rushabh Shah <shahrushabh87@gmail.com>
2020-05-09 19:51:39 +0800

    Adding Header component, Bootstrap and SCSS configuraion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  componentTitle = "I am AppComponent from the component.ts"
+   clickHandler(){
+   	alert('I am clicked');
+   }
 }
