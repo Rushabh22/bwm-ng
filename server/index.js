995f7dcd28de8e5258967585627012f902558722
Rushabh Shah <shahrushabh87@gmail.com>
2020-05-10 17:55:39 +0800

    Async and synchronizing the cleaning and adding data

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config/dev');
 const Rental = require('./models/rental');
+const FakeDb = require('./fake-db');
 
-mongoose.connect(config.DB_URI);
+mongoose.connect(config.DB_URI).then(() => {
+	const fakeDb = new FakeDb();
+	fakeDb.seedDb();
+});
 
 const app = express();
 
