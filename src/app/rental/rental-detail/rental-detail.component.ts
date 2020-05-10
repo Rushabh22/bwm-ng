5a10b6001e1492f4a1d3a62f9850b90bf950561e
Rushabh Shah <shahrushabh87@gmail.com>
2020-05-10 08:36:49 +0800

    Service to get Rental by Id

--- src/app/rental/rental-detail/rental-detail.component.ts
+++ src/app/rental/rental-detail/rental-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RentalService } from '../shared/rental.service';
+import { Rental } from '../shared/rental.model';
 
 @Component({
   selector: 'bwm-rental-detail',
@@ -9,16 +11,29 @@ import { ActivatedRoute } from '@angular/router';
 export class RentalDetailComponent implements OnInit {
    currentId: string;
 
-  constructor(private route: ActivatedRoute) { }
+   rental: Rental;
+
+  constructor(private route: ActivatedRoute,
+  			  private rentalService: RentalService) { }
 
   ngOnInit() {
   	this.route.params.subscribe(
   		(params) => {
   			console.log(params);
   			this.currentId = params['rentalId'];
+  			this.getRental(this.currentId);
   		}
-  	)
+  	  )
+   }
 
-  }
+  	getRental(rentalId: string){
+  		this.rentalService.getRentalById(rentalId).subscribe(
+  			(rental: Rental) => {
+  				this.rental = rental;
+  			}
+  		);
+  	}
+
+  
 
 }
